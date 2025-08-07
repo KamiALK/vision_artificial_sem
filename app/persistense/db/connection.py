e1fba143553f91204bcdddc12d467acfc2927546
Tu Nombre <andresyalca@gmail.com>
2025-08-07 16:06:20 -0500

    condifuracion de mongo

---
+++ app/persistense/db/connection.py
@@ -0,0 +1,8 @@
+# aqui tenemos como es la creacion de base de datos
+#
+#
+# https://github.com/KamiALK/spartanv2/blob/gregory/fastapi/db/conection.py
+#
+# por favor observa como re sealiza la conexion
+# requiere un archivo .env
+# reemplazar el valor de .env.example y quitar el.example
